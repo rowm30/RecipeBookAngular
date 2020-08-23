3791cb105f52281df87a2b649d121b9b87e8a42d
Kumar Mayank Verma <rowm30@gmail.com>
2020-08-23 10:16:07 +0530

    This becoming way simpler!

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe('A test recipe','This is only for test','https://c1.wallpaperflare.com/preview/739/376/852/kyllingefrikadeller-potatoes-meatballs-recipes.jpg')
+  ];
 
   constructor() { }
 
